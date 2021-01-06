db66f062e6c3cda485896ec2d57f216b5f697b88
Anne Villarete <annekamillev@gmail.com>
2021-01-05 21:19:34 -0800

    Removed variables and restructed return statements

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -9,89 +9,69 @@
  */
 
 function isUnderFive(number) {
-  let result;
-  var underFive = number < 5;
-  if ((number < 5)) {
-    result = underFive;
+  if (number < 5) {
+    return true;
   } else {
-    result = underFive;
+    return false;
   }
-  return result;
 }
 
 function isEven(number) {
-  let result;
-  var even = (number % 2 === 0);
-  if ((number % 2 === 0)) {
-    result = even;
+  if (number % 2 === 0) {
+    return true;
   } else {
-    result = even;
+    return false;
   }
-  return result;
 }
 
 function startsWithJ(string) {
-  let result;
-  var withJ = (string[0] === 'J');
-  if ((string[0] === 'J')) {
-    result = withJ;
+  if (string[0] === 'J') {
+    return true;
   } else {
-    result = withJ;
+    return false;
   }
-  return result;
 }
 
 function isOldEnoughToDrink(person) {
-  let result;
-  var oldEnoughToDrink = (person.age >= 21);
   if ((person.age >= 21)) {
-    result = oldEnoughToDrink;
-  } else if ((person.age < 21)) {
-    result = oldEnoughToDrink;
+    return true;
+  } else {
+    return false;
   }
-  return result;
 }
 
 function isOldEnoughToDrive(person) {
-  let result;
-  var oldEnoughToDrive = (person.age >= 16);
-  if ((person.age >= 16)) {
-    result = oldEnoughToDrive;
-  } else if ((person.age < 16)) {
-    result = oldEnoughToDrive;
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
   }
-  return result;
 }
 
 function isOldEnoughToDrinkAndDrive(person) {
-  var oldEnoughToDrinkAndDrive = 'false';
-  return oldEnoughToDrinkAndDrive;
+  return false;
 }
 
 function categorizeAcidity(pH) {
-  let result;
   if ((pH === 7)) {
-    result = 'neutral';
+    return 'neutral';
   } else if ((pH >= 0 && pH < 7)) {
-    result = 'acid';
+    return 'acid';
   } else if ((pH > 7 && pH <= 14)) {
-    result = 'base';
+    return 'base';
   } else {
-    result = 'invalid pH level';
+    return 'invalid pH level';
   }
-  return result;
 }
 
 function introduceWarnerBro(name) {
-  let result;
   if (name === 'wakko') {
-    result = "We're the Warner Brothers!";
+    return "We're the Warner Brothers!";
   } else if (name === 'yakko') {
-    result = "We're the Warner Brothers!";
+    return "We're the Warner Brothers!";
   } else if (name === 'dot') {
-    result = "I'm cute~";
+    return "I'm cute~";
   } else {
-    result = 'Goodnight everybody!';
+    return 'Goodnight everybody!';
   }
-  return result;
 }
