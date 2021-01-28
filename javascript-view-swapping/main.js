38253028335de0c04395f96f058eaef4e90013b5
annekamillev <annevillarete@users.noreply.github.com>
2021-01-28 13:15:42 -0800

    Completed JS View Swapping

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,28 @@
+var $container = document.querySelector('.tab-container');
+var $tab = document.querySelectorAll('.tab');
+var $view = document.querySelectorAll('.view');
+
+$container.addEventListener('click', function (event) {
+
+  if (!event.target && event.target.matches('.tab')) {
+       return;
+     }
+       for (var i = 0; i < $tab.length; i++) {
+         if ($tab[i] === event.target) {
+           $tab[i].className = 'tab active';
+          } else {
+            $tab[i].className = 'tab';
+      }
+    }
+
+var $dataView = event.target.getAttribute('data-view');
+
+for (var i = 0; i < $view.length; i++) {
+  if ($view[i].getAttribute('data-view') !==$dataView) {
+    $view[i].className = 'view hidden';
+  } else {
+    $view[i].className = 'view';
+  }
+}
+
+});
